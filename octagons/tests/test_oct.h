1be58dcf9d849b7666e6a08e8a755f558dde3fd1
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-07 15:35:45 +0000

    added the tests for widening and for precise glb/lub

--- octagons/tests/test_oct.h
+++ octagons/tests/test_oct.h
@@ -9,6 +9,10 @@
 #define MIN_NBCONS 1
 #define MAX_NBCONS 3
 
+//for widening
+#define K 3
+#define R(i) i < K
+
 oct_t* create_octagon(ap_manager_t* man, oct_t * top,
 		char * octagonNumber, unsigned short int dim);
 
---
+++ octagons/tests/test_oct26.c
@@ -0,0 +1,29 @@
+#include <time.h>
+#include <klee/klee.h>
+#include "oct.h"
+#include "oct_internal.h"
+#include "test_oct.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	unsigned short int dim;
+	make_symbolic_dimension(&dim);
+
+	ap_manager_t * man = oct_manager_alloc();
+	oct_t * top = oct_top(man, dim, 0);
+	oct_t * bottom = oct_bottom(man, dim, 0);
+
+	oct_t* octagon1 = create_octagon(man, top, "1", dim);
+	oct_t* octagon2 = create_octagon(man, top, "2", dim);
+	oct_t* glb = oct_meet(man, false, octagon1, octagon2);
+
+	oct_t* bound = create_octagon(man, top, "possible bound", dim);
+
+	//meet == glb, join == lub
+	//meet is the greatest lower bound
+	klee_assume(oct_is_leq(man, bound, octagon1));
+	klee_assume(oct_is_leq(man, bound, octagon2));
+	klee_assert(oct_is_leq(man, bound, glb));
+	return 0;
+}
