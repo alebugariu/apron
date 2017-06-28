619b95f8f56cedcf6c141295d754f876f62cc5db
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-28 13:44:11 +0000

    updated the results

--- parse.py
+++ parse.py
@@ -2,20 +2,50 @@
 
 import csv
 
+def dump_result(writer, results):
+	if results is not None:
+		results['status'] = (
+			"PASS"
+			if results['completed paths'] == results['generated tests']
+			else "FAIL")
+		print(results)
+		writer.writerow([results[column] for column in columns])
+
 with open('results.csv', 'w') as results_fp:
 	writer = csv.writer(results_fp)
 	columns = [
+	    'test case',
+	    'execution time (seconds)',
+	    'execution time (minutes)',
 		'completed paths',
 		'generated tests',
 		'total instructions',
+		'status',
+		'error message',
+		'output directory'
 		]
 	writer.writerow(columns)
 	for test_file in ['results.txt']:
-		results = {}
+		results = None
 		with open(test_file) as fp:
 			for line in fp:
-				if line.startswith('KLEE: done: '):
+				if line.startswith('KLEE: output directory is '):
+				    value = line.replace('KLEE: output directory is ', '').strip()
+				    dump_result(writer, results)
+				    results = {}
+				    results['error message']= "-"
+				    results['output directory'] = value[1:-1]
+				elif line.startswith('KLEE: done: '):
 					key, value = line.split(' = ')
 					key = key.replace('KLEE: done: ', '')
 					results[key] = int(value.strip())
-		writer.writerow([results[column] for column in columns])
+				elif line.startswith('Execution time: '):
+				   line = line.replace('Execution time: ', '').strip();
+				   values = line.split(" ");
+				   results['execution time (seconds)'] = int(values[0])
+				   results['execution time (minutes)'] = float(values[0]) / 60
+				   results['test case'] = values[-1]
+				elif 'KLEE: ERROR: ' in line:
+					ind = line.index('KLEE: ERROR: ')
+					results['error message'] = line[ind:].strip()
+		dump_result(writer, results)
