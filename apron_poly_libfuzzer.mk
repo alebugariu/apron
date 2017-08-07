9916bf9ed6ca548617cdd693bfaec1066567f698
Alexandra Bugariu <alexandra_bugariu@yahoo.com>
2017-08-07 15:04:58 +0000

    improved the makefile script

--- apron_poly_libfuzzer.mk
+++ apron_poly_libfuzzer.mk
@@ -1,13 +1,16 @@
 APRON=/home/apron/apron
 POLY=/home/apron/newpolka
 NUM=/home/apron/num
-INCLUDES=-I $(APRON) -I /home/apron/itv -I $(NUM) 
+ITV=/home/apron/itv
+INCLUDES=-I $(APRON) -I $(ITV) -I $(NUM) 
 POLY_INCLUDES=$(INCLUDES) -I $(POLY)
-LIBS=-L $(APRON) -L $(POLY)
+LIB_PATHS=-L $(ITV)
+LIBS=-lmpfr -lgmp -litvIl -litvIll -litvRl -litvRll -litvMPZ -litvMPQ -litvDl -litvMPFR
 FILES=$(filter-out newpolka/test.c newpolka/test0.c newpolka/test1.c newpolka/test_environment.c, $(wildcard newpolka/*.c))
 SRC_FILES=$(subst newpolka/, ,$(FILES))
+ITV_FILES=itv.c itv_linearize.c itv_linexpr.c
 APRON_OBJS=$(APRON)/ap_manager.o $(APRON)/ap_generic.o $(APRON)/ap_dimension.o $(APRON)/ap_generator0.o $(APRON)/ap_tcons0.c $(APRON)/ap_texpr0.o $(APRON)/ap_lincons0.o $(APRON)/ap_linexpr0.o $(APRON)/ap_linearize.o $(APRON)/ap_coeff.o $(APRON)/ap_scalar.o $(APRON)/ap_interval.o 
-OBJS=$(APRON_OBJS) $(POLY)/tests/libFuzzer/test_poly.o $(POLY)/*.o /home/apron/itv/*.o
+OBJS=$(APRON_OBJS) $(POLY)/tests/libFuzzer/test_poly.o $(POLY)/*.o
 
 .PHONY: compile test
 
@@ -16,25 +19,33 @@ compile:
 	cd newpolka ; \
 	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -c -g $(INCLUDES)  $(SRC_FILES) -DNUM_LONGLONGRAT ; \
 	cd .. ; \
+	cd itv ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_LONGINT -shared -fPIC -o libitvIl.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_LONGLONGINT -shared -fPIC -o libitvIll.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_LONGRAT -shared -fPIC -o libitvRl.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_LONGLONGRAT -shared -fPIC -o libitvRll.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_MPZ  -shared -fPIC -o libitvMPZ.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_MPQ -shared -fPIC -o libitvMPQ.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_DOUBLE -shared -fPIC -o libitvD.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_LONGDOUBLE -shared -fPIC -o libitvDl.so $(ITV_FILES) ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -I $(NUM) -I $(APRON) -DNUM_MPFR -shared -fPIC -o libitvMPFR.so $(ITV_FILES) ; \
+	cd .. ; \
 	cd apron ; \
-	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -c -g $(INCLUDES) -DNUM_LONGDOUBLE *.c ; \
+	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -c -g $(INCLUDES) -DNUM_LONGDOUBLE *.c; \
 	rm -f test_texpr0.o ; \
 	cd .. ; \
-	cd itv ; \
-	clang -fsanitize=address -fsanitize-coverage=trace-pc-guard -O0 -c -g -I $(NUM) -I $(APRON) -DNUM_MPZ *.c ; \
-	rm -f test.o test2.o *_debug.o ; \
-	cd ..
+	
 
 test:
 	cd newpolka/tests/libFuzzer; \
 	clang -fsanitize-coverage=trace-pc-guard $(POLY_INCLUDES) -O0 -c -g test_poly.c -DNUM_LONGLONGRAT; \
         number=$(start) ; while [ $${number} -le $(number) ] ; do \
-		clang -lstdc++ -fsanitize=address -fsanitize-coverage=trace-pc-guard -DNUM_DOUBLE -I /usr/local/include $(POLY_INCLUDES) $(OBJS) $(LIBS) test_poly$${number}.c /home/libFuzzer.a -o test$${number} -lmpfr -lgmp; \
-                startTime=`date +%s` ; \
+		clang -lstdc++ -fsanitize=address -fsanitize-coverage=trace-pc-guard -DNUM_DOUBLE -I /usr/local/include $(POLY_INCLUDES) $(OBJS) $(LIB_PATHS) test_poly$${number}.c /home/libFuzzer.a -o test$${number} $(LIBS) ; \
+        startTime=`date +%s` ; \
 		./test$${number} -max_len=10000 -detect_leaks=0 -rss_limit_mb=8192 -timeout=3600 -print_final_stats=1 MY_CORPUS/ SEED_CORPUS/; \
-                endTime=`date +%s` ; \
-                runtime=`expr $$endTime - $$startTime` ; \
-                echo "Execution time: $$runtime sec for test_poly$$number\n" ; \
+        endTime=`date +%s` ; \
+        runtime=`expr $$endTime - $$startTime` ; \
+        echo "Execution time: $$runtime sec for test_poly$$number\n" ; \
 		number=`expr $$number + 1` ; \
     	done; \
         true
